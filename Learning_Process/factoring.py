06c74b4678db65e838880f342c342498a09b9d2b
Zack <gfpss10153@gmail.com>
2024-03-25 19:15:43 +0800

    first release

---
+++ Learning_Process/factoring.py
@@ -0,0 +1,23 @@
+'''
+#range()=>得到一個範圍
+for i in range(2, 99999999999999999999999999999999999999999999999999999999):
+    print (i)
+
+'''
+'''
+z=19876
+
+print (int (z/100))
+'''
+
+z=999
+ddd=True
+
+for k in range(1,int (z/2)+1): #int() => 小數轉整數
+    if z%k==0: # %=>求餘數
+         print (k, int(z/k))
+     
+    
+#print("Is Prime=" + str(ddd)) #str=> 轉字串
+
+
